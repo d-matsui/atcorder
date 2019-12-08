5fb10b2c27e1e5fadd29b64039001108f18f26fc
Daiki Matsui <mti.daiki@gmail.com>
2019-12-08 16:44:13 +0900

    add tutorial

---
+++ tutorial/practice-1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# input number
+a = int(input())
+# input number with space
+b, c = map(int, input().split())
+# input string
+s = input()
+
+print("{} {}".format(a+b+c, s))
