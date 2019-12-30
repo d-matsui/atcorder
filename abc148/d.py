5cfe4786ac67972333ad5ecf9f5bf5c5fc06ddbb
Daiki Matsui <mti.daiki@gmail.com>
2019-12-30 14:23:55 +0900

    refactor abc148 d

--- abc148/d.py
+++ abc148/d.py
@@ -3,24 +3,23 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-flag = False
 a_copy = a
+is_possible = 1 in a_copy
 num = 0
 i = 0
-while True:
-    num += 1
-    if num in a_copy:
-        if num == 1:
-            flag = True
-        # print(num, 'is in a_copy', a_copy.index(num))
-        a_copy = a_copy[a_copy.index(num):]
-        # print('a_copy', a_copy)
-    else:
-        # print('break')
-        # print('num = ', num)
-        break
+if is_possible:
+    while True:
+        num += 1
+        if num in a_copy:
+            # print(num, 'is in a_copy', a_copy.index(num))
+            a_copy = a_copy[a_copy.index(num):]
+            # print('a_copy', a_copy)
+        else:
+            print('break')
+            # print('num = ', num)
+            break
 
-if flag:
+if is_possible:
     print(len(a) - (num - 1))
 else:
     print(-1)
