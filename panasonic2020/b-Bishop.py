e5662e72ec285a8cad34fac820ec176b65f05420
Daiki Matsui <mti.daiki@gmail.com>
2020-03-14 23:09:12 +0900

    fix b, c

--- panasonic2020/b-Bishop.py
+++ panasonic2020/b-Bishop.py
@@ -4,7 +4,12 @@ import math
 
 H, W = map(int, input().split())
 
-if H % 2 == 1 and W % 2 == 1:
+# ポイント: コーナーケースを避けるため、制約の下限に注意する。
+# 今回だと、H == 1 or W == 1のとき、必ず1になってしまう。
+
+if H == 1 or W == 1:
+    print(1)
+elif H % 2 == 1 and W % 2 == 1:
     print(math.ceil(H * W / 2))
 else:
     print(int(H * W / 2))
--- panasonic2020/c-Sqrt-Inequality.py
+++ panasonic2020/c-Sqrt-Inequality.py
@@ -4,12 +4,12 @@ import math
 
 a, b, c = map(int, input().split())
 
-# if math.sqrt(a) + math.sqrt(b) < math.sqrt(c):
-#     print("Yes")
-# else:
-#     print("No")
+# ポイント: 誤差を回避するため、整数で計算する
+# sqrt(a) + sqrt(b) < sqrt(c)
+# \equiv 2 * sqrt(a * b) < c - a - b (and c - a - b > 0)
+# \equiv 4 * a * b < (c - a - b)^2 (and c - a - b > 0)
 
-if a + b - c + (2 * math.sqrt(a * b)) < 0:
+if 4 * a * b < (c - a - b) ** 2 and c - a - b > 0:
     print("Yes")
 else:
     print("No")
