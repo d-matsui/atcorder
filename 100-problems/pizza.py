836da9e93cb1808a0f7e90ea1e7918ab6add74a6
Daiki Matsui <mti.daiki@gmail.com>
2020-03-17 23:27:22 +0900

    add twp problems using binary-search

--- 100-problems/pizza.py
+++ 100-problems/pizza.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
 
-N = 100
-l = list(range(N))
+len_ring = int(input())
+n_shops = int(input())
+m_order = int(input())
 
-def binary_search(arr, x):
-    left = 0
-    right = len(arr) - 1
-    while left <= right:
-        mid = int((left + right) / 2)
-        if arr[mid] < x:
-            left = mid + 1
-        elif arr[mid] > x:
-            right = mid - 1
-        else:
-            return mid
-    return None
+shops = [0, len_ring]
+for i in range(n_shops-1):
+    shops.append(int(input()))
+# shops = sorted(shops)
+shops.sort()
 
-print(binary_search(l, 101))
+orders = []
+for i in range(m_order):
+    orders.append(int(input()))
 
 def binary_search_closest(arr, value):
     left = 0
@@ -38,7 +34,7 @@ def binary_search_closest(arr, value):
     # left == right + 1, so arr[right] < value < arr[left]
     diff_left = arr[left] - value
     diff_right = value - arr[right]
-    # return indevalue closest to value
+    # return index closest to value
     return left if diff_left < diff_right else right
 
 def binary_search_closest_recursive(arr, value, left, right):
@@ -56,3 +52,22 @@ def binary_search_closest_recursive(arr, value, left, right):
         return binary_search_closest_recursive(arr, value, left, mid - 1)
     else:
         return mid
+
+sum = 0
+for order in orders:
+    # 配達先に最も近い店舗
+    index = binary_search_closest(shops, order)
+    # left = 0
+    # right = n_shops - 1
+    # index = binary_search_closest_recursive(shops, order, left, right)
+    start = shops[index]
+    end = order
+    if start <= end:
+        dist_clockwise = end - start
+        dist_counter_clockwise = len_ring - (end - start)
+    else:
+        dist_clockwise = len_ring - (start - end)
+        dist_counter_clockwise = start - end
+    sum += min(dist_clockwise, dist_counter_clockwise)
+
+print(sum)
