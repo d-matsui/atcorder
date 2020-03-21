7adaca42b2ce2452fcb0b74e76a98ea66d392361
Daiki Matsui <mti.daiki@gmail.com>
2020-03-21 13:58:27 +0900

    solve by using trisect search

--- 100-problems/b-moores-law.py
+++ 100-problems/b-moores-law.py
@@ -6,8 +6,10 @@ p = float(input())
 def f(x):
     return x + p / 2 ** (x / 1.5)
 
+
 def f_prime(x):
     return 1 - 0.462098 * (2 ** (-0.666667 * x)) * p
+
 
 def bs(func, value):
     left = 0.0
@@ -21,6 +23,28 @@ def bs(func, value):
             right = mid
     return mid
 
-x = bs(f_prime, 0)
-# print(x)
+
+def trisect_minimul(func):
+    left = 0
+    right = 100
+    while abs(right - left) > 0.000000001:
+        d = right - left
+        x1 = left + d/3
+        x2 = right - d/3
+        # print(f"d, x1, x2: {d}, {x1}, {x2}")
+        if func(x1) < func(x2):
+            right = x2
+        elif func(x1) > func(x2):
+            left = x1
+        else:
+            return left
+    return left
+
+
+x = trisect_minimul(f)
+print(x)
 print(f(x))
+
+# x = bs(f_prime, 0)
+# # print(x)
+# print(f(x))
