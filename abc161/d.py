9b93cfec9f5418e3986766b321e69d0c91ec8c13
Daiki Matsui <mti.daiki@gmail.com>
2020-04-05 01:25:51 +0900

    make abc161 d readable

--- abc161/d.py
+++ abc161/d.py
@@ -12,10 +12,19 @@ for _ in range(k):
     # print(f"que: {que}")
     x = que.pop()
     # print(f"x: {x}\n")
-    if x % 10 != 0:
+    r_10 = x % 10
+    if r_10 == 0:
+        # 10, 100, 1000, ...
+        que.appendleft(x * 10)
+        que.appendleft(x * 10 + 1)
+    elif r_10 == 9:
+        # 9, 99, 989, 999, ...
         que.appendleft(x * 10 + x % 10 - 1)
-    que.appendleft(x * 10 + x % 10)
-    if x % 10 != 9:
+        que.appendleft(x * 10 + x % 10)
+    else:
+        # 1, 2, ..., 8, 11, 12, 21, 22, 23, ...
+        que.appendleft(x * 10 + x % 10 - 1)
+        que.appendleft(x * 10 + x % 10)
         que.appendleft(x * 10 + x % 10 + 1)
 
 print(x)
