fe3f8c736d6f2b0e84fe61e2d5725a2c26efdad6
Daiki Matsui <mti.daiki@gmail.com>
2020-04-27 23:07:07 +0900

    add 78-planetary-exploration

--- 100-problems/78-planetary-exploration.py
+++ 100-problems/78-planetary-exploration.py
@@ -5,14 +5,14 @@ from collections import deque, defaultdict
 import sys
 
 sys.setrecursionlimit(10 ** 6)
-inf = float("inf")
+input = sys.stdin.buffer.readline
 
 m_rows, n_cols = map(int, input().split())
 k_regions = int(input())
 
 field = []
 for _ in range(m_rows):
-    line = list(input())
+    line = list(input().decode().rstrip())
     field.append(line)
 
 regions = []
@@ -20,22 +20,30 @@ for _ in range(k_regions):
     a, b, c, d = map(int, input().split())
     regions.append([a - 1, b - 1, c - 1, d - 1])
 
-pprint(field)
-pprint(regions)
+# pprint(field)
+# pprint(regions)
 
-# s[i + 1][j + 1][x] := (0, 0)から(i, j)の領域にあるxの数
+# s[i][j][x] := [0, x) \times [0, y) の長方形区間にあるxの数
+# s[i+1][j+1][x] = s[i][j+1] + s[i+1][j] - s[i][j] + a[i][j]
 # x = {0, 1, 2}, 0: Jungle, 1: Ocean, 2: Ice
 cum_sum = [[[0 for x in range(3)] for j in range(n_cols + 1)] for i in range(m_rows + 1)]
 
 for i in range(m_rows):
     for j in range(n_cols):
+        for x in range(3):
+            cum_sum[i+1][j+1][x] = cum_sum[i][j+1][x] + cum_sum[i+1][j][x] - cum_sum[i][j][x]
         if field[i][j] == 'J':
             x = 0
         elif field[i][j] == 'O':
             x = 1
         else:
             x = 2
-        print(f"i = {i}, j = {j}, x = {x}")
-        cum_sum[i + 1][j + 1][x] = cum_sum[i][j + 1][x] + cum_sum[i + 1][j][x] - cum_sum[i][j][x] + 1
+        cum_sum[i+1][j+1][x] += 1
+# pprint(cum_sum)
 
-pprint(cum_sum[1][2])
+for i_1, j_1, i_2, j_2 in regions:
+    res = []
+    for x in range(3):
+        r = cum_sum[i_2+1][j_2+1][x] - cum_sum[i_2+1][j_1][x] - cum_sum[i_1][j_2+1][x] + cum_sum[i_1][j_1][x]
+        res.append(r)
+    print(*res)
