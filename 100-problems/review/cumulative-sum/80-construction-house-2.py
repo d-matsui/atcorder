cda5f7da95150edb4e9c9209d704d7072a7a5bb5
Daiki Matsui <mti.daiki@gmail.com>
2020-07-28 23:51:28 +0900

    refactor

--- 100-problems/review/cumulative-sum/80-construction-house-2.py
+++ 100-problems/review/cumulative-sum/80-construction-house-2.py
@@ -12,38 +12,39 @@ for _ in range(H):
     line = list(map(int, input().split()))
     A.append(line)
 
-# s[i][j] = (0, 0) ~ (i-1, j-1) の領域の地価の総和
+# s[i][j] = [0, i) \times [0, j) の領域の地価の総和
 # 0 <= i <= H, 0 <= j <= W
 # s[0][0] = 0
-# s[i][j] = s[i][j-1] + s[i-1][j] + A[i-1][j-1] - s[i-1][j-1]
+# s[i+1][j+1] = s[i+1][j] + s[i][j+1] + A[i][j] - s[i][j]
 
 s = [[0 for j in range(W+1)] for i in range(H+1)]
-for i in range(1, H+1):
-    for j in range(1, W+1):
-        s[i][j] += A[i-1][j-1]
+for i in range(H):
+    for j in range(W):
+        s[i+1][j+1] = A[i][j]
 
-for i in range(1, H+1):
-    for j in range(1, W+1):
-        s[i][j] += s[i][j-1] + s[i-1][j] - s[i-1][j-1]
+for i in range(H):
+    for j in range(W):
+        s[i+1][j+1] += s[i+1][j] + s[i][j+1] - s[i][j]
+# pprint(s)
 
-# (i_1, j_1) ~ (i_2, j_2) の領域の地価の総和
-# s[i_2-1][j_2-1] - s[i_1][j_2] - s[i_2][j_1] + s[i_1][j_1]
+# [i_start, i_end) \times [j_start, j_end) の領域の地価の総和
+# 0 <= i_start < i_end <= H, 0 <= j_start < j_end <= W
+# s[i_end][j_end] - s[i_start][j_end] - s[i_end][j_start] + s[i_start][j_start]
 
-def can_construct(i_1, j_1, i_2, j_2, V, K):
-    # print(i_1, j_1, i_2, j_2)
-    cost_buy = s[i_2][j_2] - s[i_1][j_2] - s[i_2][j_1] + s[i_1][j_1]
-    cost_build = (i_2 - i_1) * (j_2 - j_1) * K
+def can_construct(i_start, i_end, j_start, j_end, V, K):
+    cost_buy = s[i_end][j_end] - s[i_start][j_end] - s[i_end][j_start] + s[i_start][j_start]
+    cost_build = (i_end - i_start) * (j_end - j_start) * K
     if cost_buy + cost_build <= V:
         return True
     return False
 
 max_area = 0
-for i_1 in range(H):
-    for j_1 in range(W):
-        for i_2 in range(i_1+1, H+1):
-            for j_2 in range(j_1+1, W+1):
-                if not can_construct(i_1, j_1, i_2, j_2, V, K):
+for i_start in range(H):
+    for i_end in range(i_start+1, H+1):
+        for j_start in range(W):
+            for j_end in range(j_start+1, W+1):
+                if not can_construct(i_start, i_end, j_start, j_end, V, K):
                     continue
-                max_area = max(max_area, (i_2 - i_1) * (j_2 - j_1))
+                max_area = max(max_area, (i_end - i_start) * (j_end - j_start))
 
 print(max_area)
